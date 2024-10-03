d8bbc2f62636b8cf956f3f05bb3accbb63f9d7b4
Favour Idowu <favouridowu02@gmail.com>
2024-10-03 02:17:27 +0100

    Added a script that contains a function named createReportObject whose parameter, employeesList, is the return value of the previous function createEmployeesObject

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,6 @@
 export default function createReportObject(employeesList) {
   const newObj = {};
   newObj.allEmployees = employeesList;
+  newObj.getNumberOfDepartments = (obj) => Object.keys(obj).length;
   return newObj;
 }
