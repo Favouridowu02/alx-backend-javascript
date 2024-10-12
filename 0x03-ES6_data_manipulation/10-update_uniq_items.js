4a10ed7207564bac9c123ae5835078b056d182cd
Favour Idowu <favouridowu02@gmail.com>
2024-10-12 21:02:48 +0100

    Add a file that contains a function  that accepts one argument: map(Map). the function changes any value that is set to 1 to 100

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,8 @@
-export default function updateUniqueItems() {
-    return;
+export default function updateUniqueItems(map = new Map()) {
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
 }
