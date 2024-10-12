5bad272657236b4beac6e89dedbf3253348be65f
Favour Idowu <favouridowu02@gmail.com>
2024-10-12 22:01:17 +0100

    Add a file that contains a function  that accepts one argument: map(Map). the function changes any value that is set to 1 to 100

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -6,3 +6,20 @@ console.log(map);
 
 updateUniqueItems(map)
 console.log(map);
+
+
+// trying some tests out
+const newMap = new Map([["girl", 3], ["boy", 2]]);
+console.log(newMap);
+
+updateUniqueItems(newMap)
+console.log(newMap);
+
+
+const newMap2 = new Set([1, 3]);
+console.log(newMap2);
+
+updateUniqueItems(newMap2)
+console.log(newMap2);
+
+
