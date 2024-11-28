dc02a5045c3b6ac17d331b3fc6932e64b55ee36a
Favour Idowu <favouridowu02@gmail.com>
2024-11-28 12:27:17 +0100

    Eslint compliant

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    const userName = process.stdin.read();
-    if (userName) {
-        process.stdout.write(`Your name is: ${userName}`);
-    }
+process.stdin.on('readable', () => {
+  const userName = process.stdin.read();
+  if (userName) {
+    process.stdout.write(`Your name is: ${userName}`);
+  }
 });
-process.stdin.on('end', function() {
-    process.stdout.write('This important software is now closing');
-})
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
+});
